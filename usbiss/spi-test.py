186899bfebb033c48f62b01066339878e984277b
gwdehaan <geertwdehaan@hotmail.nl>
2018-03-02 20:52:58 +0100

    spi-test

--- usbiss/spi-test.py
+++ usbiss/spi-test.py
@@ -1,3 +1,5 @@
 from spi import SPI
 
-t=SPI('COM3', spi_mode = 1, freq = 25000)
+# t=SPI('COM3', spi_mode = 1, freq = 25000)
+# TypeError: __init__() got an unexpected keyword argument 'spi_mode'
+t=SPI('COM3', 1, 25000)
